f2d31e3f7927c995fe04be89741d2c86be62c821
Laurent <l.treuil@eanet.cloud>
2022-04-13 15:30:37 +0200

    home

---
+++ js/auto-brand.js
@@ -0,0 +1,50 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 8,
+    centeredSlides: false,
+    spaceBetween: 0,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".brands-slider-arrow-left",
+        prevEl: ".brands-slider-arrow-right",
+    },
+});
+
+var appendNumber = 4;
+var prependNumber = 1;
+document
+    .querySelector(".prepend-2-slides")
+    .addEventListener("click", function(e) {
+        e.preventDefault();
+        swiper.prependSlide([
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+        ]);
+    });
+document
+    .querySelector(".prepend-slide")
+    .addEventListener("click", function(e) {
+        e.preventDefault();
+        swiper.prependSlide(
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+        );
+    });
+document
+    .querySelector(".append-slide")
+    .addEventListener("click", function(e) {
+        e.preventDefault();
+        swiper.appendSlide(
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+        );
+    });
+document
+    .querySelector(".append-2-slides")
+    .addEventListener("click", function(e) {
+        e.preventDefault();
+        swiper.appendSlide([
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+        ]);
+    });

7d5d4ed8ad925f3f761ed6c486c84d8f4b922af6
Laurent <l.treuil@eanet.cloud>
2022-04-29 12:41:10 +0200

    some js

--- js/counter.js
+++ js/counter.js
@@ -1,34 +1,18 @@
-var numbers = document.querySelectorAll('.number');
+var numbers = document.querySelector('.number')
+var numberTop = numbers.getBoundingClientRect().top
+var start = +number.innerHTML
+var end = +number.dataset.max
 
 numbers.forEach((number) => {
-    var numberTop = number.getBoundingClientRect().top;
-    // var start = +number.innerHTML;
-    // var end = +number.dataset.max;
-    // const speed = 100;
-    // var inc = Math.ceil(end / speed);
-    // console.log(inc)
-    var updateNumber = () => {
-        var end = +number.dataset.max;
-        var start = +number.innerHTML;
-        var speed = 250;
-        var inc = end / speed
-
-        if (start < end) {
-            start = `${Math.ceil(start + inc)}`
-            setTimeout(updateNumber, 1)
-        }
-    }
     window.addEventListener('scroll', function onScroll() {
         if (window.pageYOffset > numberTop - window.innerHeight / 2) {
             this.removeEventListener('scroll', onScroll);
-            // var interval = setInterval(function() {
-            //     number.innerHTML = ++inc;
-            //     console.log(inc)
-            //     if (inc == end) {
-            //         clearInterval(interval);
-            //     }
-            // }, 1);
-            updateNumber();
+            var interval = setInterval(function() {
+                number.innerHTML = ++start;
+                if (start == end) {
+                    clearInterval(interval);
+                }
+            }, 5);
         }
     });
-})
+});

38f34e80028e8839d67d35284840aaf335be8151
Laurent <l.treuil@eanet.cloud>
2022-05-03 11:34:20 +0200

    counter auto inc done on home page

--- js/counter.js
+++ js/counter.js
@@ -1,18 +1,23 @@
 var numbers = document.querySelectorAll('.number')
 
 numbers.forEach((number) => {
-    var start = +number.innerHTML
-    var end = +number.dataset.max
     var numberTop = number.getBoundingClientRect().top
     window.addEventListener('scroll', function onScroll() {
         if (window.pageYOffset > numberTop - window.innerHeight / 2) {
             this.removeEventListener('scroll', onScroll);
-            var interval = setInterval(function() {
-                number.innerHTML = ++start;
-                if (start == end) {
-                    clearInterval(interval);
+            var updateCounter = () => {
+                var start = +number.innerText
+                var end = +number.dataset.max
+                var speed = 250
+                var inc = Math.ceil(end / speed)
+                if (start < end) {
+                    number.innerText = start + inc
+                    setTimeout(updateCounter, 2)
+                } else {
+                    number.innerText = end
                 }
-            }, 1);
+            }
+            updateCounter()
         }
     });
 });
